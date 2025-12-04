135daf6a6da7cd4540740620c99473c64c3ff505
fillipe.monteiro <plaays49@gmail.com>
2025-12-04 13:50:43 -0300

    Landing Page

---
+++ app/Livewire/RecipeModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RecipeModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.recipe-modal');
+    }
+}
